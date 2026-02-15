1b700e87798e5abf9ac5e410d9d09051c1099a10
Konstantin <Glefa86@mail.ru>
2026-02-15 17:43:41 +0300

    Add files via upload

---
+++ DZ_Array_Serch.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <Windows.h>
+
+int SerchArray(int* arr, int dot, int size) {
+	int quant, i;	
+	int left = 0;
+	int right = size - 1;
+	while (left<right) {
+		i = (left + right) / 2;
+		if (arr[i]==dot) {
+			while (arr[i + 1] == dot) { 
+				i++; 
+			};
+			quant = size - (i + 1);
+			return quant;
+		} else if (arr[i] > dot) {
+			right = i - 1;
+		} else {
+			left = i + 1;
+		};
+	};
+	if (arr[left] == dot) {
+		quant = size-(left+1);
+	}
+	else if (arr[left] > dot) {
+		quant = size - left;
+	}
+	else { quant = size - (left + 2); };
+	return quant;
+}
+
+int main() {
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+	setlocale(LC_ALL, "RU");
+
+	int arr[] = {14, 16, 19, 32, 32, 32, 56, 69, 72};
+	int size = sizeof(arr) / sizeof(arr[0]);
+	int dot;
+	std::cout << "Введите точку отсчёта : ";
+	std::cin >> dot;
+	std::cout << "Колличество элементов в массиве больше чем " << dot << " : " << SerchArray(arr, dot, size) << "\n";
+
+	return 0;
+}
