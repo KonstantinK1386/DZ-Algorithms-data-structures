743017619fabd7545f3149b7f25b7dd3d5e52aab
Konstantin <Glefa86@mail.ru>
2026-02-24 22:53:10 +0300

    Add files via upload

---
+++ DZ_NumbersFibonacci.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+int NumFibonacci(int n)
+{
+	if (n == 1)
+		return 0;
+	else if (n == 2)
+		return 1;
+	else
+		return NumFibonacci(n - 1) + NumFibonacci(n - 2);
+}
+
+int main() {
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+	setlocale(LC_ALL, "RU");
+
+	int n;
+	std::cout << "Введите колличество чисел Фибоначчи : ";
+	std::cin >> n;
+	std::cout << NumFibonacci(n)<< "\n";
+
+
+	return 0;
+}
